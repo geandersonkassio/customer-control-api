b2100a467a49b1f82a71ea9ee5c7d49c542bfc3b
Geanderson Kassio <geankassiosilva@gmail.com>
2021-12-05 18:31:01 -0300

    controller and routes of customer

---
+++ app/Http/Controllers/API/CustomerController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Customer;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+
+class CustomerController extends Controller
+{
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'phone' => 'required',
+            'cpf' => 'required',
+            'license_plate' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'Validation error',
+                'data' => $validator->errors()
+            ], 400);
+        }
+
+        $customer = new Customer;
+        $customer->fill($request->all());
+        $customer->save();
+
+        return response()->json($customer, 201);
+    }
+
+    public function show($id)
+    {
+        $customer = Customer::find($id);
+
+        if (!$customer) {
+            return response()->json([
+                'message' => 'Record not found'
+            ], 404);
+        }
+
+        return response()->json($customer);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $customer = Customer::find($id);
+
+        if (!$customer) {
+            return response()->json([
+                'message' => 'Record not found'
+            ], 404);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'phone' => 'required',
+            'cpf' => 'required',
+            'license_plate' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'Validation error',
+                'data' => $validator->errors()
+            ], 400);
+        }
+
+        $customer->fill($request->all());
+        $customer->save();
+
+        return response()->json($customer);
+    }
+
+    public function destroy($id)
+    {
+        $customer = Customer::find($id);
+
+        if (!$customer) {
+            return response()->json([
+                'message' => 'Record not found'
+            ], 404);
+        }
+
+        $customer->delete();
+    }
+
+    public function searchByLicensePlateLastNumber($lastNumber)
+    {
+        $customers = Customer::where(DB::raw('substring(placa_carro, -1)'), '=', $lastNumber)->get();
+
+        if ($customers->isEmpty()) {
+            return response()->json([
+                'message' => 'No records found'
+            ], 404);
+        }
+
+        return response()->json($customers);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\API\CustomerController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,3 +13,9 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::post('/cliente', [CustomerController::class, 'store']);
+Route::put('/cliente/{id}', [CustomerController::class, 'update']);
+Route::get('/cliente/{id}', [CustomerController::class, 'show']);
+Route::delete('/cliente/{id}', [CustomerController::class, 'destroy']);
+Route::get('/consulta/final-placa/{numero}', [CustomerController::class, 'searchByLicensePlateLastNumber']);
